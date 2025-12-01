c5678f275226fe231b9866b646b5852b98e69d13
Maksym Prysyazhny <maksymprysyazhny@icloud.com>
2025-11-30 19:34:11 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@ int main()
 
   Gradebook gradebook;
 
+  gradebook.print_report();
+
   gradebook.add_student("Bob Bobberson", "ABC123");
   gradebook.add_student("Sam Sammerson", "DEF456");
   gradebook.add_student("Jess Jesserson", "HIJ789");
@@ -16,6 +18,8 @@ int main()
   gradebook.add_assignment("Lab 1", 50);
 
   gradebook.enter_grade("Sam Sammerson", "Quiz 1", 95);
+    gradebook.enter_grade("Sam Sammerson", "Quiz 3434", 95);
+
   gradebook.enter_grade("Bob Bobberson", "Quiz 1", 85);
   gradebook.enter_grade("Jess Jesserson", "Lab 1", 49);
   gradebook.enter_grade("Jess Jesserson", "Quiz 1", 93);
